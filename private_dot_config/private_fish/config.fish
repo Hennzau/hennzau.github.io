b367d3060be3042952050906bd3ec3c01f339557
Enzo Le Van <dev@enzo-le-van.fr>
2025-06-23 16:43:54 +0200

    macos fish env

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,3 +4,10 @@ starship init fish | source
 
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/.cargo/bin"
+
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
